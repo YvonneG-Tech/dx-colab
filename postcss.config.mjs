e248ff3ab0cecd918bc1b63073d64c96f51acb05
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-25 15:06:14 +0800

    msg

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,4 +2,21 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
+      content: [
+        // Include paths to your template files where Tailwind classes are used
+        '.pages/**/*.{ts,tsx}',
+        './components/**/*./{ts,tsx}',
+        './app/**/*./{ts,tsx}',
+        './src/**/*.{js,jsx,ts,mjs,md,json,css,tsx}',
+        './public/index.html',
+      ],
+      theme; {
+        extend: {
+          // Customize your theme here (colors, fonts, etc.)
+        }
+      }
+      plugins: [
+        // Add any Tailwind plugins here
+      ]
+
 export default config;
