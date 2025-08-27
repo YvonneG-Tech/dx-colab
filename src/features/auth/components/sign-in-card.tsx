feb6b110db7021ad82a41276580807d2ea9c038c
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-28 03:23:17 +0800

    sign in card

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,7 +1,12 @@
+import {Card, CardHeader} from "@/components/ui/card";
+
 export const SignInCard = () => {
     return (
-        <div>
+        <Card className="w-full h-full p-8">
+            <CardHeader className="px-0 pt-0">
+                Login to continue
+            </CardHeader>
             Sign In Card
-        </div>
+        </Card>
     );
 };
