44b138b1355dbf19cf2a15fdbaa0f6833a48d9f8
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-28 03:46:04 +0800

    submit form

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 
@@ -15,7 +16,7 @@ export const SignInCard = () => {
             </CardDescription>
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5">
-                  <Input
+                <Input
                    disabled={false}
                    value=""
                    onChange={() => {}}
@@ -23,6 +24,17 @@ export const SignInCard = () => {
                    type="email"
                    required
                   />
+                  <Input
+                   disabled={false}
+                   value=""
+                   onChange={() => {}}
+                   placeholder="Password"
+                   type="password"
+                   required
+                  />
+                  <Button type="submit" className="w-full" size="lg" disabled={false}>
+                    Continue
+                  </Button>
                 </form>
 
             </CardContent>
