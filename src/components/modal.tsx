91af7e872b3a2672746568f0b0d872771ec6ab79
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-30 03:51:12 +0800

    m

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,7 @@
 // src/components/modals.tsx
 "use client";
 
-import { CreateWorkspaceModal } from "@../components/create-workspace-modal";
-
+import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
 export const Modals = () => {
   return (
     <>
