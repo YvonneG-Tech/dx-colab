fadfaa618e2a39177b78da68d7d61f903746a515
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-29 23:00:22 +0800

    forked files

--- convex/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAuthenticated = exports.store = exports.signOut = exports.signIn = exports.auth = void 0;
-var server_1 = require("@convex-dev/auth/server");
-exports.auth = (_a = (0, server_1.convexAuth)({
-    providers: [],
-}), _a.auth), exports.signIn = _a.signIn, exports.signOut = _a.signOut, exports.store = _a.store, exports.isAuthenticated = _a.isAuthenticated;
--- convex/http.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("convex/server");
-var auth_1 = require("./auth");
-var http = (0, server_1.httpRouter)();
-auth_1.auth.addHttpRoutes(http);
-exports.default = http;
