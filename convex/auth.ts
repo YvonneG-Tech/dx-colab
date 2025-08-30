cdd000cf59539d21d3ceabf39862eeb51b6fe41b
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-31 01:24:37 +0800

    m

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,17 +1,7 @@
-// convex/auth.ts
 import { convexAuth } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
 
 export const { auth, signIn, signOut, store } = convexAuth({
   providers: [Password],
 });
-
-const authConfig = {
-  providers: [
-    {
-      id: "password"
-    }
-  ],
-};
-
-export default authConfig;
+// c:\Users\yvonn\dx-colab\convex\auth.ts
