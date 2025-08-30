edd2a347c14675afdaecb2c70224e87724dfc2e7
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-31 01:29:18 +0800

    m

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "http://localhost:3000",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
+// c:\Users\yvonn\dx-colab\convex\auth.config.ts
