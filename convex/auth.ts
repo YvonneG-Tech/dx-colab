5f3f08b7f5c2f5b7335a8c2b1f9b2a8e5926ee3b
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-08-30 21:45:18 +0800

    m

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,4 +1,3 @@
-import Google from "@auth/core/providers/google";
 import { convexAuth } from '@convex-dev/auth/server';
 import { Password } from "@convex-dev/auth/providers/Password";
 import { DataModel } from './_generated/dataModel';
@@ -13,5 +12,5 @@ const CustomPassword = Password<DataModel>({
 })
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [CustomPassword, Google]
+  providers: [CustomPassword]
 });
