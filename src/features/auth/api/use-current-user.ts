deeea3179174ddb7b126ed9ba72dfb6d07db3c1f
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-09-14 07:54:20 +0800

    m

--- src/features/auth/api/use-current-user.ts
+++ src/features/auth/api/use-current-user.ts
@@ -2,7 +2,7 @@ import { useQuery } from "convex/react"
 import { api } from "../../../../convex/_generated/api"
 
 export const useCurrentuser = () => {
-    const data = useQuery(api.users.current);
+    const data = useQuery(api.users.currentUser);
     const isLoading = data === undefined;
     return { data, isLoading }
 }
