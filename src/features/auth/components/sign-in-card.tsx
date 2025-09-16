95b83fba091f3cd8c3c2b95b597ebaaf05853da3
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-09-16 16:36:48 +0800

    m

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,98 +1,119 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { useAuthActions } from '@convex-dev/auth/react';
+import { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { SignInFlow } from '../types';
+import { TriangleAlert } from 'lucide-react';
 
-import { useAuthActions } from "@convex-dev/auth/react"
-import { FcGoogle } from "react-icons/fc"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
-import { SignInFlow } from "../types"
-import { useState } from "react"
-import { TriangleAlert } from "lucide-react"
 interface SignInCardProps {
+  // eslint-disable-next-line no-unused-vars
   setState: (state: SignInFlow) => void;
 }
+
 export const SignInCard = ({ setState }: SignInCardProps) => {
-  const [email, setEmail] = useState('');
   const { signIn } = useAuthActions();
+
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [pending, setPending] = useState(false);
   const [error, setError] = useState('');
-  const onPasswordSignIn = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const [pending, setPending] = useState(false);
+
+  const handlePasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     setPending(true);
-    signIn("password", { email, password, flow: "signIn" }).catch(() => {
-      setError("Invalid Email or Password")
-    }).finally(() => {
-      setPending(false);
-    })
-  }
-  const onProviderSignIn = (value: "google" ) => {
+    signIn('password', { email, password, flow: 'signIn' })
+      .catch(() => {
+        setError('Invalid email or password');
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
+  const handleProviderSignIn = (value: 'github' | 'google') => {
     setPending(true);
     signIn(value).finally(() => {
-      setPending(false)
-    })
+      setPending(false);
+    });
   };
+
   return (
-    <Card className="w-full h-full p-8">
+    <Card className="h-full w-full p-8">
       <CardHeader className="px-0 pt-0">
-        <CardTitle>
-          Login to Continue
-        </CardTitle>
+        <CardTitle>Log in to continue</CardTitle>
         <CardDescription>
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
       {!!error && (
-        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+        <div className="mb-6 flex items-center gap-x-2 rounded-md bg-destructive/15 p-3 text-sm text-destructive">
           <TriangleAlert className="size-4" />
           <p>{error}</p>
         </div>
       )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
+        <form
+          onSubmit={handlePasswordSignIn}
+          className="space-y-2.5"
+        >
           <Input
             disabled={pending}
             value={email}
-            onChange={(e) => { setEmail(e.target.value) }}
-            type="email"
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
+            type="email"
             required
           />
           <Input
             disabled={pending}
             value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
-            onChange={(e) => { setPassword(e.target.value) }}
             type="password"
             required
           />
           <Button
             type="submit"
-            className="w-full"
+            size="lg"
             disabled={pending}
-            size={"lg"}
+            className="w-full"
           >
             Continue
           </Button>
-          <Separator />
-          <div className="flex flex-col gap-y-2.5">
-            <Button
-              disabled={pending}
-              variant={"outline"}
-              size={"lg"}
-              onClick={() => { onProviderSignIn("google") }}
-              className="w-full relative"
-            >
-              <FcGoogle className="size-5 absolute top-2.5 left-2.5" />
-              Continue with Google
-            </Button>
-
-          </div>
-          <p className="text-xs text-muted-foreground">
-            Don&apos;t have an account? <span onClick={() => setState("signUp")} className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
-          </p>
         </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            onClick={() => handleProviderSignIn('google')}
+            variant="outline"
+            size="lg"
+            disabled={pending}
+            className="relative w-full"
+          >
+            <FcGoogle className="absolute left-2.5 top-3 size-5" />
+            Continue with Google
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?{' '}
+          <span
+            onClick={() => setState('signUp')}
+            className="cursor-pointer text-sky-700 hover:underline"
+          >
+            Sign up
+          </span>
+        </p>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,43 +1,59 @@
-"use client";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage
-} from "@/components/ui/avatar"
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useCurrentuser } from "../api/use-current-user";
-import { Loader, LogOut } from "lucide-react";
-import { useAuthActions } from "@convex-dev/auth/react";
+} from '@/components/ui/dropdown-menu';
+import { Loader, LogOut } from 'lucide-react';
+import { useCurrentuser } from '../api/use-current-user';
+import { useAuthActions } from '@convex-dev/auth/react';
+
 export const UserButton = () => {
   const { signOut } = useAuthActions();
+
   const { data, isLoading } = useCurrentuser();
-  if (isLoading) return <Loader className="size-4 animate-spin text-muted-foreground" />
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
   if (!data) {
     return null;
   }
-  const { name, image } = data;
+
+  const { image, name } = data;
+
   const avatarFallback = name!.charAt(0).toUpperCase();
+
   return (
     <DropdownMenu modal={false}>
-      <DropdownMenuTrigger className="outline-none relative">
-        <Avatar className="size-10 hover:opacity-75 transition rounded-md">
-          <AvatarImage className="rounded-md" alt={name} src={image} />
-          <AvatarFallback className="bg-sky-500 text-white rounded-md">
+      <DropdownMenuTrigger className="relative outline-none">
+        <Avatar className="size-10 transition hover:opacity-75">
+          <AvatarImage
+            alt={name}
+            src={image}
+          />
+          <AvatarFallback className="bg-sky-500 text-white">
             {avatarFallback}
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="center" side="right" className="w-60">
-        <DropdownMenuItem className="h-10" onClick={()=>signOut()}>
-          <LogOut className="size-4 mr-2" />
-          Logout
+      <DropdownMenuContent
+        align="center"
+        side="right"
+        className="w-60"
+      >
+        <DropdownMenuItem
+          onClick={() => signOut()}
+          className="h-10"
+        >
+          <LogOut className="mr-2 size-4" />
+          Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
