1731511dbb750e4d811895e5b4e2f00ba147f03b
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-09-17 18:33:51 +0800

    m

--- src/features/workspaces/api/use-join.ts
+++ src/features/workspaces/api/use-join.ts
@@ -1,6 +1,11 @@
 import { useQuery } from 'convex/react';
 import { api } from '../../../../convex/_generated/api';
-
+import { useMutation
+  
+ } from 'convex/react';
+export const useJoin = () => {
+  return useMutation(api.workspaces.join);
+};
 export const useGetWorkspaces = () => {
   const data = useQuery(api.workspaces.get);
   const isLoading = data === undefined;
@@ -8,6 +13,3 @@ export const useGetWorkspaces = () => {
   return { data, isLoading };
 };
 
-export function useJoin(): { mutate: unknown; isPending: unknown; } {
-  throw new Error("Function not implemented.");
-}
