408acc142e696cb2978552c9d51a70e2455c3c14
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-09-17 21:30:26 +0800

    m

--- src/components/Convex-Client-Provider.tsx
+++ src/components/Convex-Client-Provider.tsx
@@ -2,10 +2,14 @@
 
 import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
-import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>;
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  );
 }
