d9ce14a3b37052018a2faeab209a2053c3f35c79
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-09-20 00:49:40 +0800

    m

--- src/features/workspaces/api/use-get-workspace.ts
+++ src/features/workspaces/api/use-get-workspace.ts
@@ -3,11 +3,18 @@ import { useQuery } from 'convex/react';
 import { api } from '../../../../convex/_generated/api';
 import { Id } from 'convex/_generated/dataModel';
 interface UseGetWorkspaceProps {
-  id: Id<'workspaces'>;
+  id: Id<'workspaces'> | undefined;
 }
 
 export const useGetWorkspace = ({ id }: UseGetWorkspaceProps) => {
-  const data = useQuery(api.workspaces.getById, { id });
+  if (!id) {
+    return {
+      data: undefined,
+      isLoading: false,
+    };
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const data = useQuery(api.workspaces.getById, { id: id!});
   const isLoading = data === undefined;
 
   return { data, isLoading };
