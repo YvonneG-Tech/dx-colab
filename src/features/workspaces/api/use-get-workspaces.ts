65661361b5c01154f7d34c05419f6182e45512ca
yardig-tech <188824178+yardig-tech@users.noreply.github.com>
2025-09-20 01:00:24 +0800

    m

--- src/features/workspaces/api/use-get-workspaces.ts
+++ src/features/workspaces/api/use-get-workspaces.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-export const useGetWorkSpaces = () => {
+export const useGetWorkspaces = () => {
     const data = useQuery(api.workspaces.get);
     const isLoading = data === undefined;
     return { data, isLoading }
